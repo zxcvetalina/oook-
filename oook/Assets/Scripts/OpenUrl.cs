2f2fc3f3d241dc7a8f2c4e35ea1eb6342cbe4a09
zxcvetalina <gamegasgas3516@gmail.com>
2022-12-27 11:45:32 +1000

    add menu

---
+++ oook/Assets/Scripts/OpenUrl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{   
+    public void UrlOpen(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
